7dc34c65dd193284a50775ea81f57da8a5b4adf2
Mayo, Sam <sdm2000@hw.ac.uk>
2024-03-31 23:17:57 +0100

    delete from WHERE function

--- delete_n_patch.py
+++ delete_n_patch.py
@@ -5,17 +5,13 @@ from utils import small_dict, build_sql_table
 def delete_rows(kept, calculated_to_keep):
     delete_string = 'DELETE FROM Technologies;\nDELETE FROM TechnologyPrereqs;\n'
     delete_string += 'DELETE FROM Technologies_XP2;\nDELETE FROM Civics;\nDELETE FROM CivicPrereqs;\n'
-    delete_string += 'DELETE FROM Civics_XP2;\nDELETE FROM Building_YieldChanges;\n'
+    delete_string += 'DELETE FROM Civics_XP2;\n'
     delete_string += 'DELETE FROM Building_GreatPersonPoints;\nDELETE FROM Unit_BuildingPrereqs;\n'
     delete_string += 'DELETE FROM UnitUpgrades;\nDELETE FROM Boosts;\n'
-    delete_string += f"DELETE FROM Units WHERE UnitType NOT IN ("
-    for unit in kept['compat_for_VI'] + kept['units_as_is']:
-        delete_string += f"'{unit}', "
-    delete_string = delete_string[:-2] + ');\n'
-    delete_string += f"DELETE FROM Buildings WHERE BuildingType NOT IN ("
-    for building in calculated_to_keep + ['BUILDING_PALACE']:
-        delete_string += f"'{building}', "
-    delete_string = delete_string[:-2] + ');\n'
+    delete_string += delete_from_gen('Units', 'UnitType', kept['compat_for_VI'] + kept['units_as_is'])
+    delete_string += delete_from_gen('Buildings', 'BuildingType', calculated_to_keep + ['BUILDING_PALACE'])
+    delete_string += delete_from_gen('Building_YieldChanges', 'BuildingType', ['BUILDING_PALACE'])
+    # delete_string += 'DELETE FROM Building_YieldChanges;'
 
     return delete_string
 
@@ -42,3 +38,10 @@ def traits_string_generate(trait_types_to_define, kinds):
     traits_string = traits_string[:-1] + ";\n"
 
     return traits_string, kinds
+
+
+def delete_from_gen(table, filter_column, kept_items):
+    delete_from_string = f"DELETE FROM {table} WHERE {filter_column} NOT IN ("
+    for unit in kept_items:
+        delete_from_string += f"'{unit}', "
+    return delete_from_string[:-2] + ');\n'
