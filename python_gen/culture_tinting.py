5e218270b71dea8e568056c87f132625e691c084
slothoth <smayobiz@gmail.com>
2024-11-02 22:40:14 +0000

    hairless orcs and demons

--- python_gen/culture_tinting.py
+++ python_gen/culture_tinting.py
@@ -1,4 +1,15 @@
 from artdef_wrangler import Artdef
 
+# need a specified cultures.artdef entry
+# need a tint specified in Units.artdef under UnitTintTypes
+
+job = {'Orcish': 'SlthBaseMale_SkinColor_Orc',
+       'Demonic': 'SlthBaseMale_SkinColor_Demon'}
+
+hairjob = {'Orcish': 'NO_HAIR',
+       'Demonic': 'NO_HAIR'}
 artdef_object = Artdef()
-artdef_object.unit_culture_artdef(artdef_object.folder)
+# artdef_object.unit_culture_artdef(artdef_object.folder, job, 'skin')
+artdef_object.unit_culture_artdef(artdef_object.folder, hairjob, 'hairs')
+
+
