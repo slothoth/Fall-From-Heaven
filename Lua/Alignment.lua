e07a6f3ece378dec777351a6b7baa84398120b34
slothoth <smayobiz@gmail.com>
2024-12-08 20:53:45 +0000

    fixed religion alignment issues

--- Lua/Alignment.lua
+++ Lua/Alignment.lua
@@ -298,45 +298,6 @@ function RespawnerSpawned(playerID, cityID, buildingID, plotID, isOriginalConstr
     end
 end
 
-function GrantReligionUnit(playerID, unitID)
-    -- deal with Encampment district issues
-    -- if somehow not in a city, its a summon, implement that later (or dont even)?
-    local pUnitAbilities
-    local sReligionAbility
-    local pPlayer = Players[playerID]
-    local pUnit =  pPlayer:GetUnits():FindID(unitID)
-    if not pUnit then print('UNIT NOT FOUND ON SPAWN BIG ERROR'); return; end
-    local iUnitType = pUnit:GetType()                       -- check that the unit doesnt have a default religion
-    if tInherentReligion[iUnitType] then return; end
-    local iX, iY = pUnit:GetLocation()
-    local pPlot = Map.GetPlot(iX, iY)
-    local pCity = Cities.GetPlotPurchaseCity(pPlot:GetIndex())
-
-    if pCity then
-        local tiReligions = City:GetReligion():GetReligionsInCity()
-        if tiReligions then
-            pUnitAbilities = pUnit:GetAbility()
-            local CHANCE_OF_GRANT_RELIGION = 15
-            for idx, val in ipairs(tiReligions) do                  --TODO needs testing to see what this table contains
-                local iAttempt = math.random(0, 99)
-                if iAttempt > CHANCE_OF_GRANT_RELIGION then
-                    local iAlignment = tReligionAlignment[val]
-                    if iAlignment then
-                        if iAlignment == 0 then
-                            pUnitAbilities:AddAbilityCount('ALIGNMENT_EVIL')   -- does this function even work.
-                        elseif iAlignment == 1 then
-                            pUnitAbilities:AddAbilityCount('ALIGNMENT_GOOD')
-                        end
-                    end
-                    sReligionAbility = tReligionAbility[val]
-                    pUnitAbilities:AddAbilityCount(sReligionAbility)
-                    break
-                end
-            end
-        end
-    end
-end
-
 function GrantReligionFromCivicCompleted(playerID, civicIndex, isCancelled)
     local iReligion = tReligousCivicTrigger[civicIndex]
     if iReligion then
@@ -597,7 +558,6 @@ end
 
 Events.UnitKilledInCombat.Add(alignmentDeath)
 GameEvents.BuildingConstructed.Add(RespawnerSpawned)
-Events.UnitAddedToMap.Add(GrantReligionUnit)                         -- test UnitAbilityGained
 LuaEvents.NewGameInitialized.Add(onStart);
 LuaEvents.NewGameInitialized.Add(InitiateReligions);
 Events.CivicCompleted.Add(GrantReligionFromCivicCompleted)
